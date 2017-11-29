2223b632a0ba451e0dcbcf05e82d4f8253ce5409
Rakshit Sareen <rakshitsareen@172-16-31-167.DYNAPOOL.NYU.EDU>
2017-11-29 16:35:13 -0500

    col1 analysis/output: col1_statistics.out

--- DataCleaning/col1.py
+++ DataCleaning/col1.py
@@ -1,10 +1,19 @@
 from pyspark import SparkContext
-from helper import *
-from operator import add
+
+
+def checkIfInvalid(x):
+    if not x.strip():
+        return (x,"INVALID")
+    return (x,"VALID")
 
 if __name__ == "__main__":
     sc = SparkContext()
-    col = fetch_column(sc, 0)
-    counts  = col.count()
-    counts.saveAsTextFile("col1_statistics.out")
+    lines = sc.textFile("crime.csv")
+    header = lines.first()
+    lines = lines.filter(lambda x: x != header)
+    lines = lines.mapPartitions(lambda x: reader(x))
+    col = lines.map(lambda x: x[0])
+    col = col.map(lambda x: checkIfInvalid(x))
+    col = col.map(lambda x: (x[1], 1)).reduceByKey(lambda a,b : a+b)
+    col.saveAsTextFile("col1_statistics.out")
     sc.stop()

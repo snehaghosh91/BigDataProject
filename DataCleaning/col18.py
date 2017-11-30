540b26cd4cb6395c10e589cc20b1c2b410dee103
snehaghosh91 <snehaghosh91@gmail.com>
2017-11-30 15:19:11 -0500

    More Scripts added

---
+++ DataCleaning/col18.py
@@ -0,0 +1,27 @@
+from pyspark import SparkContext
+from helper import *
+from operator import add
+
+if __name__ == "__main__":
+	sc = SparkContext()
+	col = fetch_column(sc, 18)
+	hadevelopt_list = get_hadevelopt(sc)
+	col_validity_map = col.map(lambda x : (x[0], validate(x[1], "HADEVELOPT", hadevelopt_list)))
+	invalid_col = col_validity_map.filter(lambda x : not x[1][1])
+	invalid_col_out = invalid_col.map(lambda x : str(x[0]) + "\t" + str(x[1][0]) + "\t" + str(x[1][2]))
+	header = sc.parallelize(["CMPLNT_NUM \t HADEVELOPT"])
+	sc.union([header, invalid_col_out]).saveAsTextFile("col18_invalid_data.out")
+	valid_col = col_validity_map.filter(lambda x : x[1][1])
+	valid_col_out = valid_col.map(lambda x : str(x[0]) + "\t" + str(x[1][0]))
+	sc.union([header, valid_col_out]).saveAsTextFile("col18_valid_data.out")
+	counts = sc.parallelize(["INVALID COUNT:\t" + str(invalid_col.count()), "VALID COUNT:\t" + str(valid_col.count())]);
+        if not invalid_col.isEmpty():
+		invalid_col = invalid_col.map(lambda x: x[1])
+		invalid_count_map = invalid_col.map(lambda x : ((x[0],x[2]), 1)).reduceByKey(add)
+		invalid_max_count = invalid_count_map.map(lambda x : x[1]).max()
+		invalid_max_occ = invalid_count_map.filter(lambda x : x[1] == invalid_max_count)
+		invalid_max_occ_out = invalid_max_occ.map(lambda x : "MAX INVALID OCCURENCE \t" + str(x[0][0]) + "\t" + str(x[0][1]) + "\t" + str(x[1]))
+		sc.union([counts, invalid_max_occ_out]).saveAsTextFile("col18_statistics.out")
+	else:
+		counts.saveAsTextFile("col18_statistics.out")
+	sc.stop()

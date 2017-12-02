8e54ab93a80ba94dd7c4c016f430de7f6a81d065
Rakshit Sareen <rs5606@nyu.edu>
2017-12-01 20:38:21 -0500

    Time correction added

--- DataCleaning/col2-4.py
+++ DataCleaning/col2-4.py
@@ -21,8 +21,27 @@ def checkTimeValidity(timestring):
     except:
         return 'INVALID'
 
+def returnCorrectedTime(timestring):
+    if not timestring.strip():
+        return timestring
+    try:
+        groups = re.search('(\d{1,2}):(\d{1,2}):(\d{1,2})', timestring)
+        if groups:
+            found1 = int(groups.group(1))
+            found2 = int(groups.group(2))
+            found3 = int(groups.group(3))
+            if found1 == 24 and found2 == 0 and found3 == 0:
+                return '00:' + groups.group(2) + ':' + groups.group(3)
+            else:
+                return timestring
+    except:
+        return timestring
+
+
+
 
 if __name__ == "__main__":
+    #print returnCorrectedDate("24:00:00")
     sc = SparkContext()
     lines = sc.textFile(sys.argv[1])
     header = lines.first()
@@ -30,16 +49,29 @@ if __name__ == "__main__":
     lines = lines.mapPartitions(lambda x: reader(x))
     fromTime = lines.map(lambda x : (x[0],x[2], checkTimeValidity(str(x[2]))))
     toTime = lines.map(lambda x: (x[0], x[4], checkTimeValidity(str(x[4]))))
+
     invalidFromTime = fromTime.filter(lambda x: x[2] == "INVALID" or x[2] == "NULL")
-    validFromTime = fromTime.filter(lambda x: x[2] != "INVALID" and x[2] != "NULL")
-    invalidFromTime.saveAsTextFile("invalidFromTime.out")
-    validFromTime.saveAsTextFile("validFromTime.out")
+    invalidFromTime.saveAsTextFile("col2_invalid.out")
+
+    validFromTime = fromTime.filter(lambda x: x[1] != "INVALID" and x[2] != "NULL")
+    validFromTime.saveAsTextFile("col2_valid.out")
+    #correct data
+    validFromTime = fromTime.filter(lambda x: x[2] != "NULL")
+    validFromTime = validFromTime.map(lambda x: (x[0], returnCorrectedTime(x[1]), "VALID"))
+    validFromTime.saveAsTextFile("col2_corrected.out")
+
     invalidToTime = toTime.filter(lambda x: x[2] == "INVALID" or x[2] == "NULL")
+    invalidToTime.saveAsTextFile("col4_invalid.out")
+
     validToTime = toTime.filter(lambda x: x[2] != "INVALID" and x[2] != "NULL")
-    invalidToTime.saveAsTextFile("invalidToTime.out")
-    validToTime.saveAsTextFile("validToTime.out")
+    validToTime.saveAsTextFile("col4_valid.out")
+    #correct data
+    validToTime = toTime.filter(lambda x: x[2] != "NULL")
+    validToTime = validToTime.map(lambda x: (x[0], returnCorrectedTime(x[1]), "VALID"))
+    validToTime.saveAsTextFile("col4_corrected.out")
+
     topInvalidValueFrom = invalidFromTime.map(lambda x:((x[1],x[2]),1)).reduceByKey(lambda a,b:a+b)
-    topInvalidValueFrom.saveAsTextFile("topInvalidValuesFromTime.out")
+    topInvalidValueFrom.saveAsTextFile("col2_statistics.out")
     topInvalidValueTo = invalidToTime.map(lambda x:((x[1],x[2]),1)).reduceByKey(lambda a,b:a+b)
-    topInvalidValueTo.saveAsTextFile("topInvalidValuesToTime.out")
+    topInvalidValueTo.saveAsTextFile("col4_statistics.out")
     sc.stop()
